9fc0580ff3bd2ac7f559d4028f5b31878f89ca5b
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 11:06:20 +0200

    df

--- app.py
+++ app.py
@@ -112,13 +112,9 @@ if st.button('Deposit Prediction 💵'):
 
     #print out result to user
     st.metric(label="Predicted answer", value=f'{predicted_value1}')
-    #if st.metric:
-        #if(predicted_value == 1):
-        #st.success('This customer segment will Deposit')
-        #else:
-        #st.success('This customer segment will NOT Deposit')  
+
     
     #print SHAP explainer to user
-        st.subheader(f'Why {predicted_value1}? See below:')
-        shap_value = explainer.shap_values(line_to_pred)
-        st_shap(shap.force_plot(explainer.expected_value, shap_value, line_to_pred), height=400, width=900)
+        #st.subheader(f'Why {predicted_value1}? See below:')
+        #shap_value = explainer.shap_values(line_to_pred)
+        #st_shap(shap.force_plot(explainer.expected_value, shap_value, line_to_pred), height=400, width=900)
