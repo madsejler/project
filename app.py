aa27aff9c20ee205da3c43cf2546a8fb7f7b2973
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 15:15:50 +0200

    df

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ st.markdown("### Live Market Information")
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
-st.subheader(print("Current Time =", current_time))
-
+st.markdown(print("Current Time =", current_time))
+st.time_input(current_time)
 st.subheader('The rate is xxx')
