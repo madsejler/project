440f7301596bd1a78b63e808675c0b8bcc485c93
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 15:44:00 +0200

    df

--- app.py
+++ app.py
@@ -69,11 +69,10 @@ for seconds in range(3):
         st.markdown("### Detailed Data View")
         st.dataframe(data)
         time.sleep(1)
-# NYT AFSNIT
+# Live Market Information. If we had more time we would like to do some scraping for a site like statistic Portugal, to get the real euribor rate. For now it's just manually writting
 st.markdown("### Live Market Information")
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 st.time_input(current_time)
-st.markdown(print("Current Time =", current_time))
 st.markdown('The euribor 3 month rate is **1.173%** right now')
 st.markdown('**4.899.800** people are employed right now')
