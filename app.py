6b7e051bf5f0ed247cb6f6ef480f915bcb2b0d79
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 15:04:42 +0200

    df

--- app.py
+++ app.py
@@ -84,10 +84,10 @@ for seconds in range(1):
     
     with placeholder.container(): 
 # create two columns
-        rate = st.columns(1)
+    rate = st.columns(1)
 
         # fill in those three columns with respective metrics or KPIs 
-        rate.metric(label="Average Age ⏳", value=(avg_rate))
+    rate.metric(label="Average Age ⏳", value=(avg_rate))
 
         fig_col1 = st.columns(1)
         with fig_col1:
