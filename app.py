2cd5f2fc937cfc818f66a3d584c2ec292af1ffc8
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 11:04:13 +0200

    df

--- app.py
+++ app.py
@@ -108,9 +108,11 @@ if st.button('Deposit Prediction 💵'):
 
     #run prediction for 1 new observation
     predicted_value = model_xgb.predict(line_to_pred)[0]
+    predicted_value1 = if predicted_value == 1: st.success('This customer segment will Deposit') else:
+        st.success('This customer segment will NOT Deposit')  
 
     #print out result to user
-    st.metric(label="Predicted answer", value=f'{predicted_value}')
+    st.metric(label="Predicted answer", value=f'{predicted_value1}')
     #if st.metric:
         #if(predicted_value == 1):
         #st.success('This customer segment will Deposit')
