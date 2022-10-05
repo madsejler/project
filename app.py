3e4264bb47c61c1dc5feb0ab5ffdd0366574e6b9
madsejler <112628488+madsejler@users.noreply.github.com>
2022-10-05 11:08:45 +0200

    df

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ if st.button('Deposit Prediction 💵'):
 
     #run prediction for 1 new observation
     predicted_value = model_xgb.predict(line_to_pred)[0]
-    predicted_value1 = dict_predicted = {1:'yes', 0:'no'}
+    predicted_value1 = {1:'yes', 0:'no'}
 
     #print out result to user
     st.metric(label="Predicted answer", value=f'{predicted_value1}')
